8f8531bea350a6281a0337b5ef8fe9f9dce6d70d
Antonio GP <antonio.garcia@appslopers.com>
2021-09-17 16:39:30 +0200

    router angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HijoComponent } from './hijo/hijo.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HijoComponent,
+    HomeComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
